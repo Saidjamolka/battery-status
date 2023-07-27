fefa80def29a8d51714b3f7cd91bef5437de13da
Saidjamolka <saidjamolpubgm@gmail.com>
2023-07-27 12:22:23 -0400

    battery_level

--- app.js
+++ app.js
@@ -1,32 +1,35 @@
-const battery_full = document.querySelector(".battery-full")
-const battery_flashing = document.querySelector(".battery-flashing")
-const battery_charging = document.querySelector(".battery-charging")
+initBattery()
 
-const text = document.querySelector(".level")
-
-const batteryStatus = document.querySelector(".status")
-
-navigator.getBattery().then((batt)=> {
-    updateBattery = () => {
-        let level = Math.floor(batt.level * 100)
-        text.innerHTML = level + '%'
-        
-        if (level == 100) {
-            batteryStatus.innerHTML = `Battareykangiz To'liq: <i class='bx bxs-battery battery-full'></i>`
-        }
-        
-        else if (level <= 15 &! batt.charging) {
-            batteryStatus.innerHTML = `Battareykangizni To'ldiring <i class='bx bxs-plug bx-flashing bx-flip-horizontal battery-flashing'></i>`
-        }
-
-        else if (batt.charging) {
-            batteryStatus.innerHTML = `Quvvat Olmoqda... <i class='bx bxs-battery-charging bx-flashing bx-flip-horizontal battery-charging'></i>`
-        }
-
-        else {
-            batteryStatus.innerHTML = ''
-        }
-    }
+function initBattery() {
+    const text = document.querySelector(".level")
+    const batteryStatus = document.querySelector(".status")
     
-    updateBattery()
-})
+    navigator.getBattery().then((batt)=> {
+        updateBattery = () => {
+            let level = Math.round(batt.level * 100)
+            text.innerHTML = level + '%'
+            
+            if (level == 100) {
+                batteryStatus.innerHTML = `Battareykangiz To'liq: <i class='bx bxs-battery battery-full'></i>`
+            }
+            
+            else if (level <= 15 &! batt.charging) {
+                batteryStatus.innerHTML = `Battareykangizni To'ldiring <i class='bx bxs-plug bx-flashing bx-flip-horizontal battery-flashing'></i>`
+            }
+            
+            else if (batt.charging) {
+                batteryStatus.innerHTML = `Quvvat Olmoqda... <i class='bx bxs-battery-charging bx-flashing bx-flip-horizontal battery-charging'></i>`
+            }
+            
+            else {
+                batteryStatus.innerHTML = ''
+            }
+        }
+        
+        updateBattery()
+        
+        batt.addEventListener("chargingchange", () => {updateBattery()})
+        batt.addEventListener("levelchange", () => {updateBattery()})
+
+    })
+}
